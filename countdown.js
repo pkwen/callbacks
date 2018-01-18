40291e9a76cd3681b568fa0d8fb55e71fa9be422
Danny K Pan <kaiwen006@gmail.com>
2018-01-18 18:06:44 +0000

    add countdown function

---
+++ countdown.js
@@ -0,0 +1,23 @@
+var countdownGenerator = function (x) {
+  return function() {
+    if(x > 0) {
+      console.log('T-minus ' + x + '...');
+      // x--;
+      // countdownGenerator = countdownGenerator(x);
+    }else if(x === 0) {
+      console.log("Blast Off!");
+    }else {
+      console.log('Rockets already gone, bub!');
+    }
+    x--;
+  }
+
+};
+
+var countdown = countdownGenerator(3);
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
